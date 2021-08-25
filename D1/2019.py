708d68d457566890f6c9a325cc4aed493b10fd60
keonho <geonho0378@daum.ent>
2021-08-25 22:31:06 +0900

    Problem D1

---
+++ D1/2019.py
@@ -0,0 +1,9 @@
+number = int(input())
+#반복문을 위한 값 초기화
+i = 0
+j = 1
+# j출력후 두 배하기 
+while i < number + 1:
+    print(j, end = ' ')
+    j *= 2
+    i += 1
---
+++ D1/2058.py
@@ -0,0 +1,5 @@
+num = str(input())
+j = 0
+for i in range(len(num)):
+    j += int(num[i])
+print(j)
